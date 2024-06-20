08ccca4da69fcf6c31deb06be4eeefca69608436
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-06-20 05:00:38 +0200

    update the api call middleware

--- redux-apicall-demo/src/store/middlewares/api.js
+++ redux-apicall-demo/src/store/middlewares/api.js
@@ -5,14 +5,14 @@ import { apiRequested, apiCallSucceeded, apiCallFailed } from '../actionCreators
 const api = baseURL => store => next => async action => {
   if (action.type !== apiRequested.type) return next(action);
 
-  //next(action); //call the next middleware only for debugging purposes (Redux DevTools)
-  const { dispatch, getState } = store;
+  next(action); //call the next middleware only for debugging purposes (Redux DevTools)
+  const { dispatch } = store;
   const { url, method, data, onSuccess, onError } = action.payload;
   try {
     const response = await axios.request({ baseURL, url, method, data });
 
     //General success dispatch
-    dispatch(apiCallSucceeded(response.data));
+    //dispatch(apiCallSucceeded(response.data));
 
     if (onSuccess)
       dispatch({ type: onSuccess, payload: response.data });
@@ -21,14 +21,11 @@ const api = baseURL => store => next => async action => {
     const { code, message } = error;
     const serializableError = `${code}: ${message}`;
 
-    console.log(serializableError);
     //General error dispatch
-    dispatch(apiCallFailed(serializableError));
-    console.log("After apiCallFailed dispatch");
+    //dispatch(apiCallFailed(serializableError));
 
     if (onError)
       dispatch({ type: onError, payload: serializableError });
-    console.log("After error dispatch");
   }
 }
 export default api;
